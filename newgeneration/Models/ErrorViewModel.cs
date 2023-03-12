21e0b77a769bb7bc57c6ef635162366c794717e8
01Nijat <mr.nijat.asgarov@gmail.com>
2023-03-12 14:21:05 +0400

    newGen

---
+++ newgeneration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace newgeneration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
